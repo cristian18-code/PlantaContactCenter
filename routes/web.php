88c0e705623ecbf281282a92e600adb56d415a41
Cristian18-code <cristiangarzon180@gmail.com>
2021-08-25 09:46:47 -0500

    cambio de ruta Home

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,6 @@ Auth::routes();
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/', 'HomeController@index')->name('home');
-    Route::get('/home', 'HomeController@index')->name('home');
     Route::resource('/collaborators', 'CollaboratorController')->names('collaborators');
 
     // collaborators
